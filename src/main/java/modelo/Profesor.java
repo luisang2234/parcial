8641cb96f0bd5d0022884f1a19607fb4ffecf3a0
luisang2234 <luis_ang.orlas@uao.edu.co>
2026-04-13 11:14:02 -0500

    creamos la clase profesor que hereda de persona

---
+++ src/main/java/modelo/Profesor.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Profesor extends Persona{
+    private String cedula;
+    private String area;
+    private double valorHora;
+    private int horasMes;
+    
+    public profesor(){
+        
+    }
+    
+    
+}
+
