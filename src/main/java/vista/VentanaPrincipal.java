e8ef3470cbfab12ba4058a89ed8714e06b9c467c
luisang2234 <luis_ang.orlas@uao.edu.co>
2026-04-13 18:32:32 -0500

    agregamos los campos de texto y el area de resultados

--- src/main/java/vista/VentanaPrincipal.java
+++ src/main/java/vista/VentanaPrincipal.java
@@ -26,6 +26,18 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         btnAgregarProfesor = new javax.swing.JButton();
         btnListarEstudiantes = new javax.swing.JButton();
         btnListarProfesores = new javax.swing.JButton();
+         txtNombre = new javax.swing.JTextField();
+        txtDireccion = new javax.swing.JTextField();
+        txtTelefono = new javax.swing.JTextField();
+        txtFechaNacimiento = new javax.swing.JTextField();
+        txtCodigo = new javax.swing.JTextField();
+        txtGrado = new javax.swing.JTextField();
+        txtPromedio = new javax.swing.JTextField();
+        txtCedula = new javax.swing.JTextField();
+        txtArea = new javax.swing.JTextField();
+        txtValorHora = new javax.swing.JTextField();
+        txtHorasMes = new javax.swing.JTextField();
+        txtResultado = new javax.swing.JTextArea();
     }
 
     
