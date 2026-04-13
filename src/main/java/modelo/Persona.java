7ccc8f0737fee0617f4c44060361cc5181a1fc5a
luisang2234 <luis_ang.orlas@uao.edu.co>
2026-04-13 10:52:24 -0500

    implementamos la clase persona con atributos y metodos get/set

---
+++ src/main/java/modelo/Persona.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+public class Persona {
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String fechaNacimiento;
+
+    public Persona() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String mostrarDatos() {
+        return "Nombre: " + nombre +
+               ", Dirección: " + direccion +
+               ", Teléfono: " + telefono +
+               ", Fecha Nacimiento: " + fechaNacimiento;
+    }
+}
