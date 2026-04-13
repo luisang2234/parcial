ea16d35fca63a3425f54f023696d0ee6d563f1ae
luisang2234 <luis_ang.orlas@uao.edu.co>
2026-04-13 16:11:11 -0500

    creamos la clase colegio para gestionar el ingreso de las personas

---
+++ src/main/java/modelo/Colegio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Colegio {
+    
+}
