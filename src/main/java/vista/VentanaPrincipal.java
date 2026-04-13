0992fadbc0045e45e3d0263b3d1309ac2489f89d
luisang2234 <luis_ang.orlas@uao.edu.co>
2026-04-13 18:05:40 -0500

    creamos la vista para poder hacer la interfaz grafica para interaccion con el usuario

---
+++ src/main/java/vista/VentanaPrincipal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+import controlador.ControladorColegio;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author LENOVO
+ */
+public class VentanaPrincipal {
+    
+}
