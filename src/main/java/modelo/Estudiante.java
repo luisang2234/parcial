66f11894d5789f4c2005c233648a866569977452
luisang2234 <luis_ang.orlas@uao.edu.co>
2026-04-13 11:02:32 -0500

    implementamos la clase Estudiante con atributos y metodos get/set

---
+++ src/main/java/modelo/Estudiante.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Estudiante extends Persona {
+    private String codigo;
+    private String grado;
+    private double promedio;
+    
+    public Estudiante(){
+    
+}
+    public String getCodigo(){
+        return codigo;
+    }
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
+    public String getGrado() {
+        return grado;
+    }
+    public void setGrado(String grado) {
+        this.grado = grado;
+    }
+    public double getPromedio() {
+        return promedio;
+    }
